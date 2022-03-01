a47414e2701fa264c09657abfb36bee3fcaa56e1
Davidwys <david.wyseur@gmail.com>
2022-03-01 16:58:09 +0100

    maj david

--- _admin_categories_liste.php
+++ _admin_categories_liste.php
@@ -3,7 +3,7 @@ require("_admin_header.php");
 ?>
 
 <main>
-    <h1>ADMIN &raquo; Accueil de l'administration du site</h1>
+    <h1>ADMIN &raquo; Gestion des catégories</h1>
     <!-- TODO : Faire des liens vers les différentes page de l'administration :
         -> _admin_articles_liste.php => gestion des articles (lister chaque article par page avec les options (modifier/supprimer))
         -> _admin_categories_liste.php => même chose pour les catégorie ;)
