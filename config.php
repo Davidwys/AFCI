cb372f3ef9bf05427108350f9ef6ac6a3f3c6fd9
Davidwys <david.wyseur@gmail.com>
2022-03-01 15:41:49 +0100

    Ajout (bis) de config.php avec quelques modifs

--- config.php
+++ config.php
@@ -12,4 +12,31 @@ return array(
         PDO::ATTR_EMULATE_PREPARES => false
     ]
 );
+
+# --------------- CONNEXION ------------- #
+$config = require("config.php");
+
+/* 
+$dsn = "mysql:host=localhost;dbname=firstCrud;charset=utf8mb4"
+*/
+$dsn = 
+    "mysql:host=".$config["host"]
+    .";dbname=".$config["db"]
+    .";charset=".$config["charset"];
+// On demande à PHP d'essayer de se connecter
+try{
+    $pdo = new PDO(
+        $dsn,
+        $config["user"],
+        $config["password"],
+        $config["options"]
+    );
+/* Si il n'y a pas d'erreur, le catch est ignoré, sinon il 
+lancera l'erreur */
+}catch(\PDOException $e){ // la variable $e doit être de type PDOException
+    // On demande à PDO Exception 
+    // d'afficher le message d'erreur et son code.
+    throw new \PDOException($e->getMessage(), (int)$e->getCode());
+}
+
 ?>
