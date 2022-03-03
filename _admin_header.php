1c39f41709c488b59a10c3bd988b923471054ffe
jeo7nord <98310871+jeo7nord@users.noreply.github.com>
2022-03-03 16:07:28 +0100

    test

--- _admin_header.php
+++ _admin_header.php
@@ -3,7 +3,7 @@ require("header.php");
 ?>
 <nav id="admNav">
     <ul>
-        <li><a href="_admin.php" class="lienM">Accueil</a></li>
+        <li><a href="_admin. php" class="lienM">Accueil</a></li>
         <li><a href="_admin_articles_liste.php" class="lienM">Articles</a></li>
         <li><a href="_admin_categories_liste.php" class="lienM">Catégories</a></li>
         <li><a href="_admin_users_liste.php" class="lienM">Utilisateurs</a></li>
