21581cc902057e448f180358cad479b89888951b
Davidwys <david.wyseur@gmail.com>
2022-03-08 21:39:37 +0100

    Corrections de connexion

--- connexion.php
+++ connexion.php
@@ -1,57 +1,6 @@
 <?php
 require("header.php");
-require("validate.class.php");
-require("user.class.php");
 
-// Si il y a une demande de déconnexion
-if(isset($_GET['v']) && $_GET['v'] == 'exit') {
-    logout();
-    // Puis on affiche un petit message à notre utilisateur
-    $error = "Merci de votre visite, à très bientôt ;)";
-    $errorClass = "no-error";
-}
-function logout() {
-    // On vide nos variable de session.
-    $_SESSION = [];
-
-    // On met fin à la session.
-    session_destroy();
-}
-
-//------------------------------------ TODO 
-
-// Si il y a une demande de connexion :
-if(isset($_POST["connexion"])) {
-    $connexion = new Validate;
-    $email = $connexion->checkEmail($_POST["email"]);
-    $password = $connexion->checkPassword($_POST["password"]);
-    var_dump($password);
-
-    if(empty($connexion->erreur)) {
-        // On identifie le visiteur :
-        // 1 - on sélectionne l'utilisateur en BDD grâce aux infos transmis avec le formulaire de connexion (e-mail et mot de passe).
-        $user = selectByEmail($email);
-        //var_dump($user);
-        if($user) {// si l'e-mail est présent en BDD
-            if(password_verify($password, $user["password"])) {
-                /* Je range dans les variables de session dans les index de mon choix, les valeurs prouvant que l'utilisateur est connecté. */
-                $_SESSION["role"] = $user["role"];
-                $_SESSION["idUser"] = $user["idUser"];
-                $_SESSION["email"] = $user["email"];
-                
-            } else {// Si le mot de passe ne correspond pas
-                $error = "Mot de passe incorrecte.";
-                $errorClass = "error";
-            }
-        } else {
-            $error = "L'utilisateur n'existe pas !";
-            $errorClass = "error";
-        }
-    } else {
-        $error = $connexion->erreur;
-        $errorClass = "error";
-    }
-}
 // Si il y a une demande d'inscription :
 if(isset($_POST["inscription"])) {// ========================[ TODO ! ]===
     echo "inscription";
