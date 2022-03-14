64a712b377b2cfb16e7978e7f0b11adcfdaedbcf
Davidwys <david.wyseur@gmail.com>
2022-03-14 17:02:58 +0100

    page administration des articles

--- _admin_articles_liste.php
+++ _admin_articles_liste.php
@@ -7,13 +7,59 @@ require("_admin_header.php");
     <!-- TODO : lister chaque article par page avec les options (modifier/supprimer) -->
 
 <?php
-function selectAllUser() {
+function selectAllArticles() {
     $pdo = connexion();
-    $sql = $pdo->query("SELECT * FROM articles ORDER BY DESC");
+    $sql = $pdo->query("SELECT * FROM articles ORDER BY nom DESC");
     return $sql->fetchAll();
 }
+$article = selectAllArticles();
+
+function selectAllCategories() {
+    $pdo = connexion();
+    $sql = $pdo->query("SELECT * FROM categories ORDER BY nom DESC");
+    return $sql->fetchAll(PDO::FETCH_GROUP|PDO::FETCH_ASSOC);
+}
+$categories = selectAllCategories();
+echo "<pre>";
+var_export($categories);
+echo "</pre>";
+// foreach($categories as $key => $value) {
+//     echo $key." = ".implode(" - ", $value[0])."<br>";
+// }
 ?>
 
+    <table>
+        <caption>LISTE DES ARTICLES</caption>
+        <thead>
+            <tr>
+                <th>Nom</th>
+                <th>Description</th>
+                <th>Catégorie</th>
+                <th></th>
+                <th></th>
+            </tr>
+        </thead>
+        <tbody>
+<?php
+foreach($article as $row) {
+?>
+            <tr>
+                <td><?php echo $row['nom']; ?></td>
+                <td><?php echo $row['description']; ?></td>
+                <td class="center"><?php echo $categories[$row['idCategorie']][0]["nom"]; ?></td>
+                <td class="center"><a href="_admin_article_edite.php?id=<?php echo $row['idArticle']; ?>&action=edite" title="Modifier"><img class="icon" src="assets/img/edite.png" alt="edite"></a></td>
+                <td class="center"><a href="#?id=<?php echo $row['idArticle']; ?>&action=delete" title="Supprimer"><img class="icon" src="assets/img/delete.png" alt="delete"></a></td>
+            </tr>
+<?php
+}
+?>
+        </tbody>
+    </table>
+
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
 
 </main>
 
