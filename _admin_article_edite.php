abb3b3c7e36a348b886b37da11439847d46b4747
Davidwys <david.wyseur@gmail.com>
2022-03-16 17:02:14 +0100

    traitement du formulaire de modification d'un article

--- _admin_article_edite.php
+++ _admin_article_edite.php
@@ -1,8 +1,47 @@
 <?php
 require("_admin_header.php");
-
+require("_fonctions.php");
 $id = false;
 $error = false;
+$color = false;
+// -----------------------------------[ TRAITEMENT DU FORMULAIRE ]-----------
+if($_POST) {
+    $error = false;
+    $id = is_numeric(trim($_GET["id"])) ? $_GET["id"] : $error = "Le format de l'identifiant de l'article est inconnu.", $color = ".error";
+    $cat = is_numeric(trim($_POST["categorie"])) ? $_POST["categorie"] : $error = "Le format de l'identifiant de la catégorie est inconnu.", $color = ".error";
+    $nom = cleanString($_POST["name"]) != "" ? cleanString($_POST["name"]) : $error = "Le champs \"Nom\" est vide.", $color = ".error";
+    $description = cleanString($_POST["description"]) != "" ? cleanString($_POST["description"]) : $error = "Le champs \"Description\" est vide.", $color = ".error";
+    //$img = ;// TODO ###################
+
+    // Vérification des champs
+    if($error) {
+        echo $error;
+    } else {// MAJ si tout est OK
+        // var_dump(
+        //     'ID = ' . $id . '<br>'.
+        //     'Catégorie = ' . $cat . '<br>'.
+        //     'Nom = ' . $nom . '<br>'.
+        //     'Description = ' . $description
+        // );
+        $pdo->prepare("UPDATE articles SET 
+                nom = :name, 
+                description = :details,
+                cat = :cat
+                WHERE idArticle = :id");
+        $sql->execute([
+            "name" => $nom,
+            "details" => $description,
+            "cat" => $cat,
+            "id" => $id
+        ]);
+        $error = "L'article a été modifié avec succès.";
+        $color = ".check";
+    }
+
+    
+
+}
+// -----------------------------------[ TRAITEMENT DU FORMULAIRE : FiN! ]----
 
 function selectArticle($i) {
     $pdo = connexion();
@@ -19,40 +58,54 @@ if(isset($_GET["id"]) && isset($_GET["action"]) && $_GET["action"] == "edite") {
 
 <main>
     <h1>ADMIN &raquo; Gestion d'un article</h1>
-
-    <table class="tabForm">
-        <caption>ARTICLES #<?php echo "id de l'article"; ?></caption>
-        <tbody>
-            <tr>
-                <td><label for="name">Nom de l'article</label> : </td>
-                <td><input type="text" name="name" id="name" value="<?php echo $article["nom"]; ?>" placeholder="Champs vide interdit !"></td>
-            </tr>
-            <tr>
-                <td><label for="description">Description</label> : </td>
-                <td><textarea name="description" id="description" placeholder="Champs vide interdit !"><?php echo $article["description"]; ?></textarea></td>
-            </tr>
-            <tr>
-                <td><label for="categorie">Catégorie</label> : </td>
-                <td>
-                    <select name="categorie" id="categorie">
-                        <option value=""></option>
-                    </select>
-                </td>
-            </tr>
-            <tr>
-                <td>Illustration : </td>
-                <td>
-                    <img src="" alt="image" class="img-mini">
-                    <p><input type="file" name="img" id="img"></p>
-                </td>
-            </tr>
-            <tr>
-                <td></td>
-                <td><input type="submit" value="Modifier"></td>
-            </tr>
-        </tbody>
-    </table>
-    
+<?php
+// Affichage d'une eventuelle erreur : 
+if($error != false) {
+    echo "<p class=\"". $color ."\">". $error ."</p>";
+}
+?>
+    <form action="#" method="post" enctype="multipart/form-data">
+        <table class="tabForm">
+            <caption>ARTICLES #<?php echo $article["idArticle"]; ?></caption>
+            <tbody>
+                <tr>
+                    <td><label for="name">Nom de l'article</label> : </td>
+                    <td><input type="text" name="name" id="name" value="<?php echo $article["nom"]; ?>" placeholder="Champs vide interdit !"></td>
+                </tr>
+                <tr>
+                    <td><label for="description">Description</label> : </td>
+                    <td><textarea name="description" id="description" placeholder="Champs vide interdit !" cols="30" rows="10"><?php echo $article["description"]; ?></textarea></td>
+                </tr>
+                <tr>
+                    <td><label for="categorie">Catégorie</label> : </td>
+                    <td>
+                        <select name="categorie" id="categorie">
+                            <?php 
+                            $pdo = connexion();
+                            $sql = $pdo->query("SELECT * FROM categories ORDER BY nom");
+                            $cato = $sql->fetchAll();
+                            foreach($cato as $cat) {
+                                $select = $cat["idCategorie"] == $article["idCategorie"] ? " selected" : "";
+                                echo '<option value="'.$cat["idCategorie"].'"'.$select.'>'.$cat["nom"].'</option>';
+                            }
+                            ?>
+                        </select>
+                    </td>
+                </tr>
+                <tr>
+                    <td>Illustration : </td>
+                    <td>
+                        <img src="<?php echo $article["image"]; ?>" class="img-mini" alt="image" class="img-mini">
+                        <p><input type="file" name="img" id="img"></p>
+                    </td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td><input type="submit" value="Modifier"></td>
+                </tr>
+            </tbody>
+        </table>
+    </form>
 <p>&nbsp;</p>
 <p>&nbsp;</p>
 <p>&nbsp;</p>
