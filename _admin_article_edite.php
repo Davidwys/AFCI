df15802b03440841ff98e984711ec344ea95a429
Davidwys <david.wyseur@gmail.com>
2022-03-17 17:08:50 +0100

    travaux sur l'image

--- _admin_article_edite.php
+++ _admin_article_edite.php
@@ -6,39 +6,75 @@ $error = false;
 $color = false;
 // -----------------------------------[ TRAITEMENT DU FORMULAIRE ]-----------
 if($_POST) {
-    //$id = is_numeric(trim($_GET["id"])) ? $_GET["id"] : $error = "Le format de l'identifiant de l'article est inconnu.", $color = "error";
+    // En cas d'erreur, je crée un tableau pour récupérer le message d'erreur et sa classe couleur ;)
     $id = is_numeric(trim($_GET["id"])) ? $_GET["id"] : $error = ["message" => "Le format de l'identifiant de l'article est inconnu.", "color" => "error"];
-    //$cat = is_numeric(trim($_POST["categorie"])) ? $_POST["categorie"] : $error = "Le format de l'identifiant de la catégorie est inconnu.", $color = "error";
     $cat = is_numeric(trim($_POST["categorie"])) ? $_POST["categorie"] : $error = ["message" => "Le format de l'identifiant de la catégorie est inconnu.", "color" => "error"];
-    //$nom = cleanString($_POST["name"]) != "" ? cleanString($_POST["name"]) : $error = "Le champs \"Nom\" est vide.", $color = "error";
     $nom = cleanString($_POST["name"]) != "" ? cleanString($_POST["name"]) : $error = ["message" => "Le champs \"Nom\" est vide.", "color" => "error"];
-    //$description = cleanString($_POST["description"]) != "" ? cleanString($_POST["description"]) : $error = "Le champs \"Description\" est vide.", $color = "error";
     $description = cleanString($_POST["description"]) != "" ? cleanString($_POST["description"]) : $error = ["message" => "Le champs \"Description\" est vide.", "color" => "error"];
-    //$img = ;// TODO ###################
+    $imgOld = cleanString($_POST["imgOld"]);
+    $imgOld = is_file($imgOld) ? $imgOld : $imgOld = false;
+    $img = false;
+
+    if(is_uploaded_file($_FILES["img"]["tmp_name"])) {
+        // Un tableau contenant les types mimes acceptés.
+        $typesPermis = ["image/png", "image/jpeg", "image/gif"];
+        //Le chemin menant au dossier où l'on rangera nos fichiers.
+        $targetDir = "assets/img/";
+
+        // basename permet de retirer les possibles chemin de dossier jusqu'au fichier ne gardant que son nom.
+        $file = basename($_FILES["img"]["name"]);
+        /* uniqid génère une valeur aléatoire sensé être unique. Les paramètres sont optionnels.
+        Le 1er permet d'ajouter un préfix, le second d'augmenter l'antropie (l'aléatoire)*/
+        $file = uniqid("", true).$file;
+        /* On concatène le nom du fichier avec le chemin où il doit être rangé.*/
+        $targetFile = $targetDir . $file;
+        // équivalent en 1 seule ligne :
+        // $file = $targetDir . uniqid(date("U"), true) . basename($_FILES["img"]["name"]);
+
+        /* mime_content_type vérifie le type du fichier.
+        On l'utilise pour éviter de croire simplement les informations envoyé par le formulaire. */
+        $mimeType = mime_content_type($_FILES["img"]["tmp_name"]);
+
+        if(file_exists($targetFile)) {
+            $error = ["message" => "Ce fichier existe déjà.", "color" => "error"];
+        }
+        if(!in_array($mimeType, $typesPermis)) {
+            $error = ["message" => "Ce type de fichier n'est pas accepté.", "color" => "error"];
+        }
+        if($_FILES["img"]["size"] > 5000000) {
+            $error = ["message" => "Ce fichier est trop lourd (supérieur à 5 MB)", "color" => "error"];
+        }
+        /* move_uploaded_file déplace un fichier téléversé depuis la zone temporaire jusqu'à son emplacement final et, retourne "true" si tout s'est bien passé sinon "false" ! */
+        $moveImg = move_uploaded_file($_FILES["img"]["tmp_name"], $targetFile);
+        $moveImg == true ? $img = $targetFile : $error = ["message" => "Le fichier n'a pas pu être téléversé.", "color" => "error"];
+
+        // Suppression de l'ancienne image
+
+    }
 
     // Vérification des champs
     if($error == false) {// MAJ si tout est OK
-        // var_dump(
-        //     'ID = ' . $id . '<br>'.
-        //     'Catégorie = ' . $cat . '<br>'.
-        //     'Nom = ' . $nom . '<br>'.
-        //     'Description = ' . $description
-        // );
-        echo "stop : != de false"; exit;
-        /*$pdo = connexion();
+        
+        $pdo = connexion();
         $sql = $pdo->prepare("UPDATE articles SET 
             nom = :name, 
             description = :details,
-            idCategorie = :cat
+            idCategorie = :cat,
+            image = :img
             WHERE idArticle = :id");
         $sql->execute([
             "name" => $nom,
             "details" => $description,
             "cat" => $cat,
-            "id" => $id
-        ]);*/
-        $error = "L'article a été modifié avec succès.";
-        $color = ".check";
+            "id" => $id,
+            "img" => $img
+        ]);
+        $error = ["message" => "L'article a été modifié avec succès.", "color" => "check"];
+        //($img !== false ? unlink($imgOld) : null);
+        if($img != false) {
+            $imgOld = end(explode("/", $imgOld));
+            unlink($imgOld);
+        }
     }
 }
 // -----------------------------------[ TRAITEMENT DU FORMULAIRE : FiN! ]----
@@ -65,7 +101,7 @@ if($error != false) {
     <form action="#" method="post" enctype="multipart/form-data">
         <table class="tabForm">
             <caption>ARTICLES #<?php echo $article["idArticle"]; ?></caption>
-            <tbody>
+            <tbody><!-- TODO : (1)Ajouter une colonne dans la table "articles" : "disponible" avec 2 états : "en stock", "rupture" et l'ajouter en option ici -->
                 <tr>
                     <td><label for="name">Nom de l'article</label> : </td>
                     <td><input type="text" name="name" id="name" value="<?php echo $article["nom"]; ?>" placeholder="Champs vide interdit !"></td>
@@ -92,9 +128,11 @@ if($error != false) {
                 </tr>
                 <tr>
                     <td>Illustration : </td>
-                    <td>
+                    <td class="center">
+                        <input type="hidden" name="imgOld" id="imgOld" value="<?php echo $article["image"]; ?>">
                         <img src="<?php echo $article["image"]; ?>" class="img-mini" alt="image" class="img-mini">
-                        <p><input type="file" name="img" id="img"></p>
+                        <p><br><input type="file" name="img" id="img"></p>
+                        <!-- TODO : (1)Personnaliser ce bouton "Parcourir" (2)Ajouter une option "supprimer image" -->
                     </td>
                 </tr>
                 <tr>
